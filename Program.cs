f83cecbea6e6b1ee67563ed61e354ced16e553fc
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-04 18:29:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //HELLO WORLD YERİNE LİON HALİL YAZDIM :)
+            Console.WriteLine("lİON HALİL");
+            Console.Write("halil");
+            Console.WriteLine("im");
+            Console.ReadLine();
+        }
+    }
+}
