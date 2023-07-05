48a9074e9e57af2698cb25c3e899d1dc42bbd386
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 00:03:58 +0300

    Add files via upload

---
+++ 03_Byte_Data_Type.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Byte_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte birinci = 5;
+
+            byte ikinci = 25;
+
+            byte ucuncu = 255; //0 - 255
+
+            byte maxDeger= byte.MaxValue;
+            byte minDeger= byte.MinValue;
+
+            Console.WriteLine(birinci);
+            Console.WriteLine(ikinci);
+            Console.WriteLine(ucuncu);
+
+            Console.WriteLine(maxDeger);
+            Console.WriteLine(minDeger);
+
+            Console.ReadLine(); 
+        }
+    }
+}
