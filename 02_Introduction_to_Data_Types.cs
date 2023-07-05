55155ac40348c2c465e54b7b9037a8db1ae2406e
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-05 22:25:05 +0300

    Add files via upload

---
+++ 02_Introduction_to_Data_Types.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_to_Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DEĞİŞKEN NASIL TANIMLANIR?
+
+            intsayi = 5;
+            //int ifadesi tam sayı tanımlamak için kullanılır.
+
+            string isim = "halil"; 
+            //string ifadesi metin degerlerini tutabilmek için kullanılır.
+
+            double ondalikli_sayi = 3.5;
+            //double ifadesi ondalıklı sayıları tanımlamak için kullanılır.
+
+            /*DEĞİŞKEN OLUŞTURURKEN DİKKAT EDİLMESİ GEREKENLER 
+            1 RAKAMLA BAŞLAMAZLAR                 --> int1sayi = 5;
+            2 BOŞLUK OLMAZ                        --> int sayı degeri = 5;
+            3 TÜRKÇE KARAKTER KULLANILMAMALIDIR.  --> intçocukSayısı = 10;
+
+        }
+    }
+}
