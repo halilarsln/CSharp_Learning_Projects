a6e777fa43b201e9b444b2e5737a3a60ccebbd69
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 10:34:26 +0300

    Add files via upload

---
+++ 10_String_Data_Type.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_String_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //çift tırnak içerisinde tanımlanır.
+            //metini tutmak için kullanılır.
+
+            string isim = "halil";
+
+            string soyisim = "arslan";
+
+            string c = "Hoşgeldiniz Arkadaşlar Dersimize :) ";
+
+            Console.WriteLine(isim);
+            Console.WriteLine(soyisim);
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
