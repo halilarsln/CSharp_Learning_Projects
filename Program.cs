0196fa521f9a827fa2a0dfa55637177ba117bf76
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 11:28:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Modify_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //mod alma operatoru : %
+
+            int sayi1 = 10;
+
+            int sayi2 = 3;
+
+            Console.WriteLine(sayi1%sayi2);
+            // 10 Rakamının 3 ile bölümünden kalanını bulduk.
+
+            Console.ReadLine();
+        }
+    }
+}
