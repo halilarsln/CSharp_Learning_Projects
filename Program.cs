a79081e76ce7e9015f11b431fcc6f20b06671bf7
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 11:37:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Increment_and_Decrease_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arttırma operatörü : ++
+            // Azaltma operatörü  : --
+
+            int sayi = 20;
+
+            sayi++; //burada bir arttırdık.
+            sayi--; //burada bir azalttık.
+
+            Console.WriteLine(sayi);
+            Console.ReadLine();
+        }
+    }
+}
