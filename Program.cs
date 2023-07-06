917d4cb30c4162a74b87a40cddb8acc76e639563
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 18:04:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Not_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Değil operatörü : !
+            //kısacası var olan bir şeyin zıttına çevirir.
+
+            bool a = true;
+            
+            Console.WriteLine(!a); // Buraya koyduğumuz ünlem ile değil olmusş oldu. 
+            Console.ReadLine(); 
+        }
+    }
+}
