4652c6e288721c2f5c775574eb64b3439faa991e
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 18:39:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_Readline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Readline
+
+            Console.WriteLine("Lütfen Adınızı Giriniz");
+
+            string isim = Console.ReadLine();
+
+            Console.WriteLine("Soyismizi giriniz");
+
+            string soyisim = Console.ReadLine();
+
+            Console.WriteLine("kullanıcın ismi:"+isim);
+            Console.WriteLine("kullanıcınım soyismi:" + soyisim);
+            Console.ReadLine();
+        }
+    }
+}
