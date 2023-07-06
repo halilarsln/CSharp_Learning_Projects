9e050d47c89d4a76eb0262434eb3c8a0e57508b1
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 12:02:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Not_Equal_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Eşit Değildir Operatörü : !=
+
+            int sayi1 = 3;
+
+            int sayi2 = 4;
+
+            bool kontrolEt = sayi1 != sayi2;
+
+            Console.WriteLine(kontrolEt);
+            Console.ReadLine();
+        }
+    }
+}
