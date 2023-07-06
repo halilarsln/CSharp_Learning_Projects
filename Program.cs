dd3af0aca1573ef43404a23c9d912652d7310b54
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 10:55:17 +0300

    12_Object_Data_Type

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Object_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //veri tiplerinin atasıdır.
+            //bütün veri tiplerini tutabilir.
+
+
+            object a = "halil";
+            object b = 155;
+            object c = '5';
+            object d = 3.14;
+            object e = true;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+            Console.ReadLine();
+
+        }
+    }
+}
