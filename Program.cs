9d2cc07f84fe8748125f2c02483eaf89cbe017b6
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 11:45:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Assignment_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama Operatörü : =
+
+            int yas = 21;
+            int kiraz = 25;
+            int muz = 16;
+
+            //hayali bir kutu oldugunu düsünelim içi boş onun içini = operatorü kullanarak yerine koyuyoruz.
+            //Sağdaki değişkeni al sola koy bu kadar :)
+
+
+        }
+    }
+}
