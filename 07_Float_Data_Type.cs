a3470fc7f7b2c241de68ae88764ed501f3103f7c
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 10:11:56 +0300

    Add files via upload

---
+++ 07_Float_Data_Type.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Float_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //float
+            //4 byte
+
+            float ondalikli_sayi1 = 35.56f;
+            //Float veri tipi olduğunu göstermek için sonuna f harfi koyulur.
+            float ondalikli_sayi2 = 12.5f;
+
+            Console.WriteLine(ondalikli_sayi1);
+            Console.WriteLine(ondalikli_sayi2);
+
+            Console.ReadLine(); 
+        }
+    }
+}
