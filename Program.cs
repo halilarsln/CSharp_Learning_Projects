e0ab1db12939ee8ae27a0635ce63e4d363434837
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 18:18:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Additional_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // +=
+            // -=
+            // /=
+            // *=
+
+            int a = 6;
+
+            // a = a + 4;
+            //a += 4; //üstteki ifadenin azaltılmıs hali
+
+            //a = a - 3;
+
+            //a = a -= 3;
+
+            // a = a * 3;
+
+            //a *= 3;
+
+            //a = a / 2;
+
+            a = a /= 2;
+
+
+
+
+
+
+            Console.WriteLine("a'nın değeri:"+a);
+            Console.ReadLine();
+
+        }
+    }
+}
