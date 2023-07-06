25c62a4afc9dc52d098609854123f5e64ccdd9c4
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 12:23:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Less_than_and_Less_than_Equal_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Küçüktür Operatörü : <
+            // Küçük Eşit Operatorü: <=
+
+            int yas = 25;
+
+            bool Sonuc = yas < 26;
+            bool bak = yas <= 25;
+
+            Console.WriteLine(Sonuc);
+            Console.WriteLine(bak);
+            Console.ReadLine();
+
+        }
+    }
+}
