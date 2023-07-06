b543b1ed842759f61d2d16e6b5fb0b70c68e18d5
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 10:18:32 +0300

    Add files via upload

---
+++ 08_Double_Data_Type.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Double_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8 byte yer kaplar
+            //15 basamaklı ondalıklı sayılarıda tutabilir.
+
+            double ondalikli_sayi1 = 350.7;
+
+            double ondalikli_sayi2 = -250.6;
+
+            Console.WriteLine(ondalikli_sayi1);
+            Console.WriteLine(ondalikli_sayi2);
+
+            Console.ReadLine();
+        }
+    }
+}
