705f0fa7fe941c6c176ca724d8db1c40b3efbbca
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 10:27:15 +0300

    Add files via upload

---
+++ 09_Char_Data_Type.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Char_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Karakterleri tutmak için Char veri tipini kullanırız.
+            //Tek tırnak içerisinde tek değer(karakter) olacak şekilde kullanılır.
+
+            char karakter1 = 'H';
+
+            char karakter2 = 'A';
+
+            char karakter3 = 'L';
+
+            char karakter4 = '?';
+
+            Console.Write(karakter1);
+            Console.Write(karakter2);
+            Console.Write(karakter3);
+            Console.WriteLine(karakter4);
+
+            Console.ReadLine();
+
+        }
+    }
+}
