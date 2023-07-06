27769da4773b257e101f2a23bbc49fc323fd3755
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-06 12:13:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Greater_and_Greater_Equal_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Büyüktür operatörü : >
+            //Büyük ve Eşittir operatörü: >=
+
+            int yaş = 17;
+
+            bool KontrolEt = yaş >= 17;
+            bool bak = yaş > 18;
+
+            Console.WriteLine(KontrolEt);
+            Console.WriteLine(bak);
+            Console.ReadLine();
+
+        }
+    }
+}
