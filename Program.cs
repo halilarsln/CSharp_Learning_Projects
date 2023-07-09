f18daa4b4419e98e36fca66c34e9893d7af263b2
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-09 15:35:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Body_Mass_Index
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Beden Kitle İndeksi = Kilo / (boy*boy)
+            //Beden Kitle İndeksi 18'e eşit veya düşükse zayıf
+            // 18 ile 25 arasında Normal
+            // 25'ten büyükse OBEZ yazdıran program parçasını kodlayınız.
+
+            Console.WriteLine("Kilonuzu Giriniz");
+            int kilo =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Boyunuzu Girişiniz");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+            double bke = kilo / (boy * boy);
+
+            if (bke<18) 
+            { 
+                Console.WriteLine("Zayıfsınız..."); 
+                Console.ReadLine();
+            }
+            else if (bke>18 && bke<25)
+            { 
+                Console.WriteLine("Normalsiniz");
+                Console.ReadLine();
+            }
+            else if (bke>25)
+            {
+                Console.WriteLine("Obezsiniz");
+                Console.ReadLine();
+            }
+        }
+    }
+}
