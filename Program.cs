4fb243d67696a2d9f33f704a7e122308321fceaa
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-12 09:05:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_Break_and_Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Break anahtar kelimesi
+            //continue anahtar kelimesi
+
+
+            /*for(int a =1; a <= 10; a++)
+            {
+                if(a == 7)
+                {
+                    break;
+                }
+                Console.WriteLine("a'nın değeri:"+a);
+
+            }
+
+            Console.ReadLine();*/
+
+            /*for (int a = 1; a <= 10; a++)
+            {
+                if (a == 7)
+                {
+                    continue; // Bir kereye mahsusu kkırıyor
+                }
+                Console.WriteLine("a'nın değeri:" + a);
+
+            }
+
+            Console.ReadLine();*/
+
+
+
+        }
+    }
+}
