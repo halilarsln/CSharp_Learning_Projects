11917059f20559b482eba93cc8f8e09fbae2524e
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-12 18:29:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_Array_Identification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string
+            //double
+            //char
+
+            //string[] isimler = new string[4]; 4 tane eleman tutabilen dizidir.
+
+            string[] isimler = { "Halil", "Şevval", "Gülgün", "Ayşe", "Mehmet" };
+
+            Console.Write(isimler[1]+isimler[0]+"birtakım");
+
+            Console.ReadLine();
+
+            for (int i = 0; i < isimler.Length; i++) //uzunlugu kadar birer birer arttır
+            {
+                Console.WriteLine(isimler[i]); //döngü bittiginde ekrana yazdıracak
+            }
+            Console.ReadLine();
+        }
+    }
+}
