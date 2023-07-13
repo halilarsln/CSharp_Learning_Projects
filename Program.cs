bf79b6146801d3c454210cc40374fd07520f817f
HALİL ARSLAN <116909407+halilarsln@users.noreply.github.com>
2023-07-13 21:13:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach döngüsü
+            
+            /*string[] isimler = { "Enes", "Aysun", "Sevcan", "Elif", "Eren" };
+              
+            /* for (int i = 0; i<isimler.Length; i++)
+            {
+            Console.WriteLine(isimler[i]);
+            }*/
+
+            /* foreach (string a in isimler)
+            {
+            Console.WriteLine(a);
+            }*/
+
+            double[] os = { 1.2, 1.4, 1.8, 2.2 };
+            foreach (double d in os)
+            {
+                Console.WriteLine(d);
+            }
+            Console.ReadLine();
+        }
+    }
+}
